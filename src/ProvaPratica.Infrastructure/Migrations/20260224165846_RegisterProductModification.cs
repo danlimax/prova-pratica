5763c822914c6d8333c15150f3cb4ff54b52740e
danlimax <daniellimaa05@gmail.com>
2026-02-24 13:59:27 -0300

    Register product modification.

---
+++ src/ProvaPratica.Infrastructure/Migrations/20260224165846_RegisterProductModification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProvaPratica.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegisterProductModification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
